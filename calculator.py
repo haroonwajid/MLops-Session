6b1a2fc29cc77e2460a61694f3cb25cf690ab96e
Haroon Wajid <130307173+haroonwajid@users.noreply.github.com>
2024-08-09 11:31:14 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,8 +4,12 @@ from unittest import result
 def add(a,b):
     return a+b
 
+def subtract(a,b)
+    return a-b
+
 if __name__ == "__main__":
-    result == add(2,3)
+    result = add(2,3)
+    result = subtract(3,2)
     print(f"the result is: {result}")
     
     
