fb92a30647475b2f0ba28a56fb829215dea8bf06
Haroon Wajid <130307173+haroonwajid@users.noreply.github.com>
2024-08-09 11:32:17 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,5 +11,7 @@ if __name__ == "__main__":
     result = add(2,3)
     result = subtract(3,2)
     print(f"the result is: {result}")
+    print(f"The result of substraction is: {result_sub}")
+    
     
     
